cef0c0d858e6470d329ffd99667dacd6b1eeaa8a
https://github.com/akkunov/print-viewmsroot <akunov313131@gmail.com>
2025-08-04 18:54:06 +0600

    не придумал что написать просто комит перед уходом

--- src/routes/Settings.tsx
+++ src/routes/Settings.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 import SettingC from '../widget/setting/Setting';
 import { useLoaderData } from 'react-router-dom';
 import { EnvelopeProfile } from 'fetch/FetchUpload';
-import EnvProfile from 'component/envProfile';
+import EnvProfile from '../component/envProfile/EnvProfile';
 
 const Settings: FC = () => {
     const profile = useLoaderData();
