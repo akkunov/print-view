9f435808e33478be00f8f5854659f3994dfcb9d8
msroot <akunov313131@gmail.com>
2026-04-28 16:27:12 +0600

    не придумал что написать просто комит перед уходом

--- src/fetch/API_URL.ts
+++ src/fetch/API_URL.ts
@@ -1,2 +1,4 @@
-export const ENVELOPEPROFILEURL: string = 'api/envelopeProfile';
-export const UPLOAD: string = 'api/upload';
+const API_URL: string = `https://print-server-yj0j.onrender.com`;
+
+export const ENVELOPEPROFILEURL: string = `${API_URL}/api/envelopeProfile`;
+export const UPLOAD: string = `${API_URL}/api/upload`;
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-// import os from 'os';
+import os from 'os';
 
-// function getLocalIP() {
-//     const interfaces = os.networkInterfaces();
-//     for (const name in interfaces) {
-//         const iface = interfaces[name];
-//         if (!iface) continue;
-//
-//         for (const net of iface) {
-//             if (net.family === 'IPv4' && !net.internal) {
-//                 return net.address;
-//             }
-//         }
-//     }
-//     return 'localhost'; // fallback
-// }
+function getLocalIP() {
+    const interfaces = os.networkInterfaces();
+    for (const name in interfaces) {
+        const iface = interfaces[name];
+        if (!iface) continue;
 
-const localIP = `https://print-server-yj0j.onrender.com/`;
+        for (const net of iface) {
+            if (net.family === 'IPv4' && !net.internal) {
+                return net.address;
+            }
+        }
+    }
+    return 'localhost'; // fallback
+}
+
+const localIP = getLocalIP();
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -26,12 +26,6 @@ export default defineConfig({
     server: {
         host: localIP,
         port: 80,
-        proxy: {
-            '/api': {
-                target: `https://print-server-yj0j.onrender.com`,
-                changeOrigin: true,
-            },
-        },
     },
     resolve: {
         alias: {
