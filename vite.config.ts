0b4a5204039c9bc74ba0b6b16afb7892aef4bfbe
msroot <akunov313131@gmail.com>
2026-04-28 14:33:03 +0600

    не придумал что написать просто комит перед уходом

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import os from 'os';
+// import os from 'os';
 
-function getLocalIP() {
-    const interfaces = os.networkInterfaces();
-    for (const name in interfaces) {
-        const iface = interfaces[name];
-        if (!iface) continue;
+// function getLocalIP() {
+//     const interfaces = os.networkInterfaces();
+//     for (const name in interfaces) {
+//         const iface = interfaces[name];
+//         if (!iface) continue;
+//
+//         for (const net of iface) {
+//             if (net.family === 'IPv4' && !net.internal) {
+//                 return net.address;
+//             }
+//         }
+//     }
+//     return 'localhost'; // fallback
+// }
 
-        for (const net of iface) {
-            if (net.family === 'IPv4' && !net.internal) {
-                return net.address;
-            }
-        }
-    }
-    return 'localhost'; // fallback
-}
-
-const localIP = getLocalIP();
+const localIP = `https://print-server-yj0j.onrender.com/`;
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -28,7 +28,7 @@ export default defineConfig({
         port: 80,
         proxy: {
             '/api': {
-                target: `http://${localIP}:3001`,
+                target: `https://print-server-yj0j.onrender.com`,
                 changeOrigin: true,
             },
         },
