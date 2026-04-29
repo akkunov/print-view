5525cca0a36ca2ae344027f86398e015473ea6b7
msroot <akunov313131@gmail.com>
2026-04-29 10:25:15 +0600

    сменил dev сервер

--- src/fetch/API_URL.ts
+++ src/fetch/API_URL.ts
@@ -1,4 +1,4 @@
-const API_URL: string = `https://print-server-yj0j.onrender.com`;
+const API_URL: string = `https://printer-api-349712343534.europe-west1.run.app`;
 
 export const ENVELOPEPROFILEURL: string = `${API_URL}/api/envelopeProfile`;
 export const UPLOAD: string = `${API_URL}/api/upload`;
